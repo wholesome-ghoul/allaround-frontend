c658f13843565d8cb16a0a6a038e50e1fe830102
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-02 20:11:06 +0400

    chore: add threejs example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+
+function _Box(props: ThreeElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 const App = () => {
   return (
-    <h1>
-      aa
-    </h1>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <_Box position={[-1.2, 0, 0]} />
+      <_Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
 };
 
