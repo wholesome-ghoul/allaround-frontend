0f12171710cbfa5fce14891f38150aba94535ee5
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-03 15:34:50 +0400

    chore: use custom configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const aaConfigsWebpack = require("@allaround/configs-webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -6,78 +7,68 @@ const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
-const outDir = "dist";
-const PORT = 3000;
+const isDev = process.env.STAGING_ENV === "dev";
 
-module.exports = function (webpackEnv) {
-  const isProd = process.env.STAGING_ENV === "prod";
-  const isDev = process.env.STAGING_ENV === "dev";
-
-  return {
-    entry: "./src/index.tsx",
-    mode: isProd ? "production" : "development",
-    output: {
-      filename: isProd ? "[name].[contenthash:8].js" : "[name].bundle.js",
-      chunkFilename: isProd
-        ? "[name].[contenthash:8].chunk.js"
-        : "[name].chunk.js",
-      filename: "[name].[contenthash].js",
-    },
-    devServer: {
-      port: PORT,
-      client: { overlay: false },
-      compress: true,
-    },
-    devtool: isDev ? "source-map" : false,
-    module: {
-      rules: [
+module.exports = () => {
+  const rules = [
+    {
+      test: /\.tsx?$/,
+      include: path.join(__dirname, "src"),
+      use: [
         {
-          test: /\.tsx?$/,
-          include: path.join(__dirname, "src"),
-          use: [
-            {
-              loader: "swc-loader",
-              options: {
-                env: { mode: "usage" },
-                jsc: {
-                  transform: {
-                    react: {
-                      runtime: "automatic",
-                      refresh: isDev,
-                    },
-                  },
+          loader: "swc-loader",
+          options: {
+            env: { mode: "usage" },
+            jsc: {
+              transform: {
+                react: {
+                  runtime: "automatic",
+                  refresh: isDev,
                 },
               },
             },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            // "style-loader",
-            "css-loader",
-          ],
+          },
         },
       ],
     },
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        // "style-loader",
+        "css-loader",
+      ],
+    },
+  ];
+
+  const plugins = [
+    isDev && new ReactRefreshWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "./index.html",
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      // filename: "static/css/[name].[contenthash:8].css",
+      // chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+    }),
+    new ESLintPlugin(),
+  ].filter(Boolean);
+
+  const rest = {
+    plugins,
     // experiments: {
     //   asyncWebAssembly: true,
     // },
-    plugins: [
-      isDev && new ReactRefreshWebpackPlugin(),
-      new ForkTsCheckerWebpackPlugin(),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        filename: "./index.html",
-        template: "./public/index.html",
-      }),
-      new MiniCssExtractPlugin({
-        // filename: "static/css/[name].[contenthash:8].css",
-        // chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-      }),
-      new ESLintPlugin(),
-    ].filter(Boolean),
-    resolve: { extensions: [".js", ".ts", ".tsx"] },
   };
+
+  const webpackConfig = aaConfigsWebpack({
+    rules,
+    rest,
+  });
+
+//   console.log(webpackConfig);
+
+  return webpackConfig;
 };
