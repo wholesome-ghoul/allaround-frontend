8fe96313c9e3ef35ebd2dc542fdad4aee609b362
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-05 20:00:51 +0400

    chore: axios example

--- src/sign-up/SignUp.tsx
+++ src/sign-up/SignUp.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import axios, { AxiosError } from "axios";
 import { Button, Label, Input, Container } from "@allaround/all-components";
 
 const _Label = styled(Label)`
@@ -11,11 +12,23 @@ const _Input = styled(Input)`
   display: block;
 `;
 
+type ServerSuccess = {
+  message: string;
+};
+
+type ServerError = {
+  error: string;
+};
+
+type ServerResponse = {
+  data: ServerSuccess & ServerError;
+};
+
 const SignUp = () => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("tmp@gmail.com");
+  const [username, setUsername] = useState("tmpusername");
   const [password, setPassword] = useState("");
 
-  // TODO: axios
   // TODO: email validator
   // TODO: password validator
 
@@ -27,12 +40,36 @@ const SignUp = () => {
     setPassword(event.target.value);
   };
 
-  const signUp = () => {
-    console.log(email, password);
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const signUp = async () => {
+    try {
+      type User = {
+        email: string;
+        username: string;
+        password: string;
+      };
+      const response = await axios.request<User, ServerResponse>({
+        method: "post",
+        url: "http://localhost:4000/api/users/sign-up",
+        data: {
+          email,
+          username,
+          password,
+        },
+      });
+      console.log(response.data.message);
+    } catch (e) {
+      const response = (e as AxiosError).response;
+      const error = (response?.data as ServerError).error;
+      console.log(error);
+    }
   };
 
   return (
-    <Container grid="3x3" gap={{ row: "1rem" }} id="noice">
+    <Container grid="3x4" gap={{ row: "1rem" }} id="noice">
       <Container noGrid gridPosition={{ rowPos: "1", colPos: "2/3" }}>
         <_Label htmlFor="email" size="medium">
           Email
@@ -46,6 +83,18 @@ const SignUp = () => {
         />
       </Container>
       <Container noGrid gridPosition={{ rowPos: "2", colPos: "2/3" }}>
+        <_Label htmlFor="username" size="medium">
+          Username
+        </_Label>
+        <_Input
+          value={username}
+          onChange={handleUsernameChange}
+          type="text"
+          id="username"
+          fill
+        />
+      </Container>
+      <Container noGrid gridPosition={{ rowPos: "3", colPos: "2/3" }}>
         <_Label htmlFor="password" size="medium">
           Password
         </_Label>
@@ -59,7 +108,7 @@ const SignUp = () => {
       </Container>
       <Button
         onClick={signUp}
-        gridPosition={{ rowPos: "3", colPos: "2/3" }}
+        gridPosition={{ rowPos: "4", colPos: "2/3" }}
         fill
       >
         Sign Up
