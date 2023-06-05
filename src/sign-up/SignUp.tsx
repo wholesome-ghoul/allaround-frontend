cd2d7cdff36f688ef5ea23fd549c2b4a91614e23
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-05 18:31:20 +0400

    chore: updates

--- src/sign-up/SignUp.tsx
+++ src/sign-up/SignUp.tsx
@@ -1,28 +1,64 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Button, Label, Input, Container } from "@allaround/all-components";
 
-const StyledLabel = styled(Label)`
+const _Label = styled(Label)`
   display: block;
   margin-bottom: 8px;
 `;
 
-const StyledInput = styled(Input)`
+const _Input = styled(Input)`
   display: block;
 `;
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // TODO: axios
+  // TODO: email validator
+  // TODO: password validator
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const signUp = () => {
+    console.log(email, password);
+  };
+
   return (
-    <Container grid="3x3" gap="1rem">
+    <Container grid="3x3" gap={{ row: "1rem" }} id="noice">
       <Container noGrid gridPosition={{ rowPos: "1", colPos: "2/3" }}>
-        <StyledLabel htmlFor="email">Email</StyledLabel>
-        <StyledInput value="" onChange={() => {}} fill />
+        <_Label htmlFor="email" size="medium">
+          Email
+        </_Label>
+        <_Input
+          value={email}
+          onChange={handleEmailChange}
+          type="email"
+          id="email"
+          fill
+        />
       </Container>
       <Container noGrid gridPosition={{ rowPos: "2", colPos: "2/3" }}>
-        <StyledLabel htmlFor="password">Password</StyledLabel>
-        <StyledInput value="" onChange={() => {}} fill />
+        <_Label htmlFor="password" size="medium">
+          Password
+        </_Label>
+        <_Input
+          value={password}
+          onChange={handlePasswordChange}
+          type="password"
+          id="password"
+          fill
+        />
       </Container>
       <Button
-        onClick={() => {}}
+        onClick={signUp}
         gridPosition={{ rowPos: "3", colPos: "2/3" }}
         fill
       >
