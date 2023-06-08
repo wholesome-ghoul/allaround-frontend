83772f2744f03312553cba813fc569f916a66902
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-08 15:43:51 +0400

    chore: use `common-scss` component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { CommonScss } from "@allaround/all-components";
 
-import "./index.scss";
+CommonScss.reset().common();
 import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
