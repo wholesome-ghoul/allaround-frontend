7bb6de3d165074123f65d44a3f9728e9a8b1fd9a
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-17 02:44:55 +0400

    chore: tags

--- src/upload-section/youtube/index.tsx
+++ src/upload-section/youtube/index.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import { Container, Textarea } from "@allaround/all-components";
+import { Container, Textarea, Tags } from "@allaround/all-components";
 
 import { theme } from "../../utils";
 
 const YoutubeUpload = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [tags, setTags] = useState([""]);
+
+  const handleTagsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTags(e.target.value.split(","));
+  };
 
   return (
     <Container grid={{ rows: "auto", cols: 12 }} styles={{ height: "unset" }}>
@@ -20,25 +25,31 @@ const YoutubeUpload = () => {
         minWidth="300px"
         autoHor
       >
-        <Container noGrid id="title-container">
-          <Textarea
-            placeholder="Add a title for your video"
-            max={100}
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            label="Title (required)"
-          />
-        </Container>
-        <Container noGrid id="description-container">
-          <Textarea
-            placeholder="Tell viewers about your video"
-            max={5000}
-            rows={15}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            label="Description"
-          />
-        </Container>
+        <Textarea
+          placeholder="Add a title for your video"
+          max={100}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          label="Title (required)"
+        />
+        <Textarea
+          placeholder="Tell viewers about your video"
+          max={5000}
+          rows={15}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          label="Description"
+          copyHandler={(e) => console.log(e)}
+          delHandler={(e) => console.log(e)}
+        />
+        <Tags
+          placeholder="Add tags"
+          max={500}
+          rows={15}
+          values={tags}
+          onChange={handleTagsChange}
+          label="Tags"
+        />
       </Container>
     </Container>
   );
